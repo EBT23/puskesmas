19b911396b1f9642f873a88f235b7352d9ed9fdc
hamdi abdul aziz <hamdiabdulaziz1@gmail.com>
2023-02-16 20:30:47 +0700

    update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,6 +11,7 @@ class DashboardController extends Controller
     {
         return view('dashboard.index');
     }
+    
     public function role()
     {
         $data['title'] = 'Role Management';
