4409fa03565c1f40800de5e9ae35f2be0efc4b13
hamdi abdul aziz <hamdiabdulaziz1@gmail.com>
2023-02-19 06:27:36 +0700

    update profile

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,9 +1,9 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="{{route('profilAdmin')}}" class="brand-link">
         <img src="{{ asset('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <span class="brand-text font-weight-light">Admin Panel</span>
     </a>
 
     <!-- Sidebar -->
@@ -11,10 +11,12 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="{{ asset('assets/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                <a href="{{route('profilAdmin')}}">
+                    <img src="{{ asset('assets/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                </a>
             </div>
             <div class="info">
-                <a href="" class="d-block">{{ Auth::user()->full_name }}</a>
+                <a href="{{route('profilAdmin')}}" class="d-block">{{ Auth::user()->full_name }}</a>
             </div>
         </div>
 
