5062b85c86e764d16026b2aa398cde231c6a80f7
aderohmatmaulana98 <aderohmatmaulana77@gmail.com>
2023-03-04 23:53:36 +0700

    style kartu pasien

--- resources/views/pendaftaran/kartuPasien.blade.php
+++ resources/views/pendaftaran/kartuPasien.blade.php
@@ -1,26 +1,56 @@
 @extends('layouts.base', ['title' => "$title - Pasien"])
 
 @section('content')
-@include('sweetalert::alert')
-<div class="content-header">
-    <div class="container-fluid">
-        <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1 class="m-0">{{ $title }}</h1>
-            </div><!-- /.col -->
-            <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active">{{ $title }}</li>
-                </ol>
-            </div><!-- /.col -->
-            
-        </div>
-        <div class="form group">
-            <div class="card-body" align="center">
-                
-                <img src="{{ asset('assets/dist/img/kartu.jpg') }}" class="img-fluid">
-                {{-- <p align="center"><b>Kartu Pasien</b></p>
+	@include('sweetalert::alert')
+	<div class="content-header">
+		<div class="container-fluid">
+			<div class="row mb-2">
+				<div class="col-sm-6">
+					<h1 class="m-0">{{ $title }}</h1>
+				</div><!-- /.col -->
+				<div class="col-sm-6">
+					<ol class="breadcrumb float-sm-right">
+						<li class="breadcrumb-item"><a href="#">Home</a></li>
+						<li class="breadcrumb-item active">{{ $title }}</li>
+					</ol>
+				</div><!-- /.col -->
+
+			</div>
+			<div class="form group">
+				<div class="card-body row">
+					<span align="center" style="position: relative; z-index: 1; top: 0px;">
+						<img src="{{ asset('assets/dist/img/kartu.jpg') }}">
+					</span>
+					<div class="row" style="position: relative; top: -270px; z-index: 2;color: #030303; margin-left: 30px; ">
+						<span>
+							<table style="border: 1px solid black;  font-size: 18px;">
+								<tr style="border: 1px solid black; padding: 10px;">
+									<th style="border: 1px solid black; padding: 10px;">No RM</th>
+									<td style="border: 1px solid black; padding: 10px;">:</td>
+									<td style="border: 1px solid black; padding: 10px; width: 90%;">Aj-9989</td>
+								</tr>
+								<tr style="border: 1px solid black; padding: 10px;">
+									<th style="border: 1px solid black; padding: 10px;">Nama</th>
+									<td style="border: 1px solid black; padding: 10px;">:</td>
+									<td style="border: 1px solid black; padding: 10px; width: 90%;">Ade Rohmat Maulana</td>
+								</tr>
+								<tr style="border: 1px solid black; padding: 10px;">
+									<th style="border: 1px solid black; padding: 10px;">NIK</th>
+									<td style="border: 1px solid black; padding: 10px;">:</td>
+									<td style="border: 1px solid black; padding: 10px; width: 90%;">3210110408980081</td>
+								</tr>
+								<tr style="border: 1px solid black; padding: 10px;">
+									<th style="border: 1px solid black; padding: 10px;">Alamat</th>
+									<td style="border: 1px solid black; padding: 10px;">:</td>
+									<td style="border: 1px solid black; padding: 10px; width: 90%;">Blok Jumaah RT/RW 001/012 Desa Burujul
+										Wetan Kec.Jatiwangi,
+										Kab. Majalengka
+									</td>
+								</tr>
+							</table>
+						</span>
+					</div>
+					{{-- <p align="center"><b>Kartu Pasien</b></p>
                 <table class="table bg-gradient-white table-borderless table-white shadow p-3 mb-5 bg-white rounded" border="3" align="center" style="width: 60%">
                 @forelse ($data->pasien as $item)
                     <tr>
@@ -72,11 +102,10 @@
                     
                     @endforelse
             </div> --}}
-        </div>
-    </div><!-- /.row -->
-</div><!-- /.container-fluid -->
-</div>
-</div>
-<!-- /.content-header -->
-
+				</div>
+			</div><!-- /.row -->
+		</div><!-- /.container-fluid -->
+	</div>
+	</div>
+	<!-- /.content-header -->
 @endsection
