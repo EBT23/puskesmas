b7c588d5b74cccc98c91d165330454010f44367f
hakim220619 <danilukman2206@gmail.com>
2023-03-05 18:40:44 +0700

    antrian

--- resources/views/antrian/view.blade.php
+++ resources/views/antrian/view.blade.php
@@ -21,21 +21,32 @@
                 </div>
                 <!-- /.card-header -->
 
-                <div class="card-body" style="height: 400px; ">
+                <div class="card-body" style="height: auto; ">
                     <div class="row">
                         @foreach ($poli as $po)
                             <form method="POST" action="{{ route('noAntrian.add') }}">
                                 @csrf
-                                <div class="card-body pt-0">
-                                    <div class="row">
-                                        <div class="col-8">
+                                <div class="card text-white bg-dark mb-3" style="max-width: auto; margin-right: 30px;">
                                     <input type="text" name="nama_poli" value="{{ $po->nama_poli }}" hidden>
                                     <input type="text" name="id_poli" value="{{ $po->id }}" hidden>
-                                    <button type="submit" class="btn btn-secondary btn-lg"
-                                        style="font-size: 300%; "><i class="fa fa-link"></i> <br>{{ $po->nama_poli }}</button>
-                                        </div>
+                                    <div class="card-body">
+                                        <button type="submit" class="btn btn-secondary btn-lg"
+                                            style="font-size: 300%; width: 400px;
+height: 180px;"><i class="fa fa-link"></i>
+                                            <br>{{ $po->nama_poli }}</button>
+                                    </div>
                                 </div>
-                            </div>
+                                {{-- <div class="card-body pt-0">
+                                    <div class="row">
+                                        <div class="col-8">
+                                            <input type="text" name="nama_poli" value="{{ $po->nama_poli }}" hidden>
+                                            <input type="text" name="id_poli" value="{{ $po->id }}" hidden>
+                                            <button type="submit" class="btn btn-secondary btn-lg"
+                                                style="font-size: 300%; "><i class="fa fa-link"></i>
+                                                <br>{{ $po->nama_poli }}</button>
+                                        </div>
+                                    </div>
+                                </div> --}}
                             </form>
                         @endforeach
                     </div>
@@ -45,31 +56,32 @@
             <section class="content">
                 <!-- Default box -->
                 <div class="card shadow card-solid">
-                    <div class="card-body pb-0">
+                    <div class="card-body">
                         <div class="row">
                             @foreach ($antrian as $ant)
-                                <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">
+                                <div class="col-md-12 d-flex align-items-stretch flex-column">
                                     <div class="card bg-light d-flex flex-fill">
                                         <div class="card-header text-muted border-bottom-0">
-                                            <h1>Poli {{$ant->nama_poli}}</h1>
+                                            <h1>Poli {{ $ant->nama_poli }}</h1>
                                         </div>
                                         <div class="card-body pt-0">
                                             <div class="row">
                                                 <div class="col-7">
                                                     <h4>Nama:</h4>
                                                     <h1 class="lead"><b>{{ $ant->full_name }}</b></h1>
-                                                    <p class="text-muted text-sm"><b>Waktu: </b> {{$ant->created_at}} </p>
-                                                    
+                                                    <p class="text-muted text-sm"><b>Waktu: </b> {{ $ant->created_at }} </p>
+
                                                 </div>
                                                 <div class="col-5 text-center">
                                                     <h1>Nomor Antrian</h1>
-                                                    <h2 style="font-size: 500%;">{{$ant->antrian}}</h2>
+                                                    <h2 style="font-size: 500%;">{{ $ant->antrian }}</h2>
                                                 </div>
                                             </div>
                                         </div>
                                         <div class="card-footer">
                                             <div class="text-right">
-                                                <a href="/cetakAntrian/{{$ant->id_antrian}}" target="_blank" class="btn btn-sm btn-primary">
+                                                <a href="/cetakAntrian/{{ $ant->id_antrian }}" target="_blank"
+                                                    class="btn btn-sm btn-primary">
                                                     <i class="fas fa-print"></i> Cetak
                                                 </a>
                                             </div>
